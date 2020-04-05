0008a62a4693cd88a5cd0d293a0fbe67e4197c8c
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-05 21:56:12 +0200

    add grid components

---
+++ src/components/grid.tsx
@@ -0,0 +1,87 @@
+import React, { FC } from 'react';
+import { css } from '@emotion/core';
+import { BP_MIN_SM, BP_MIN_MD, BP_MIN_LG, BP_MIN_XL } from '../breakpoints';
+
+type ColProps = {
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  smOffset?: number;
+  mdOffset?: number;
+  lgOffset?: number;
+  xlOffset?: number;
+};
+
+export const Row: FC = ({ children }) => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex: 0 1 auto;
+        flex-direction: row;
+        flex-wrap: wrap;
+        margin-left: -15px;
+        margin-right: -15px;
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const Col: FC<ColProps> = ({
+  sm,
+  md,
+  lg,
+  xl,
+  smOffset = 0,
+  mdOffset = 0,
+  lgOffset = 0,
+  xlOffset = 0,
+  children = 0
+}) => {
+  const media: { [k: string]: string } = {
+    sm: BP_MIN_SM,
+    md: BP_MIN_MD,
+    lg: BP_MIN_LG,
+    xl: BP_MIN_XL
+  };
+  const dimensions = { sm, md, lg, xl };
+  const offsets: { [k: string]: number } = {
+    sm: smOffset,
+    md: mdOffset,
+    lg: lgOffset,
+    xl: xlOffset
+  };
+
+  return (
+    <div
+      css={css`
+        display: block;
+        flex: 0 0 100%;
+        padding-right: 15px;
+        padding-left: 15px;
+        ${Object.entries(dimensions)
+          .filter(([, value]) => Boolean(value))
+          .map(([breakpoint, value]) => {
+            const width = (100 / 12) * (value as number);
+            return `
+            ${media[breakpoint]} {
+              flex-basis: ${width}%;
+              max-width: ${width}%;
+              display: block;
+              ${
+                offsets[breakpoint]
+                  ? `margin-left: ${(100 / 12) * offsets[breakpoint]}%`
+                  : ''
+              }
+            }
+          `;
+          })}
+      `}
+    >
+      {children}
+    </div>
+  );
+};
