e9984949fd0f46016035585f61c00a3e424a0093
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-05 21:56:26 +0200

    use section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -43,62 +43,64 @@ const Home = () => {
   const theme: { [k: string]: string } = useTheme();
 
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        height: auto;
-        margin-top: 3rem;
-        ${BP_MIN_MD} {
-          flex-direction: row;
-        }
-        ${BP_MIN_XL} {
-          height: calc(100vh - 3rem - 15px);
-        }
-      `}
-    >
-      <TitleSection
-        textColor={theme.textColorLight}
+    <section>
+      <div
         css={css`
-          background: ${theme.backgroundDark};
-          & > div {
-            flex: 1;
+          display: flex;
+          flex-direction: column;
+          height: auto;
+          margin-top: 3rem;
+          ${BP_MIN_MD} {
+            flex-direction: row;
+          }
+          ${BP_MIN_XL} {
+            height: calc(100vh - 3rem - 15px);
           }
         `}
       >
-        <Container>
-          <h1>
-            Not
-            <br />
-            Paramount
-          </h1>
-          <p>Recording and production service in Lewes, UK</p>
-          <CTALink to="/">Get in touch</CTALink>
-        </Container>
-      </TitleSection>
-      <div
-        css={css`
-          flex: 1;
-          position: relative;
-        `}
-      >
-        <Image
-          fluid={data.studioImage.childImageSharp.fluid}
-          draggable={false}
-          style={{ height: '100%' }}
-        />
+        <TitleSection
+          textColor={theme.textColorLight}
+          css={css`
+            background: ${theme.backgroundDark};
+            & > div {
+              flex: 1;
+            }
+          `}
+        >
+          <Container>
+            <h1>
+              Not
+              <br />
+              Paramount
+            </h1>
+            <p>Recording and production service in Lewes, UK</p>
+            <CTALink to="/">Get in touch</CTALink>
+          </Container>
+        </TitleSection>
         <div
           css={css`
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            background: rgba(25, 8, 0, 0.2);
+            flex: 1;
+            position: relative;
           `}
-        />
+        >
+          <Image
+            fluid={data.studioImage.childImageSharp.fluid}
+            draggable={false}
+            style={{ height: '100%' }}
+          />
+          <div
+            css={css`
+              position: absolute;
+              top: 0;
+              bottom: 0;
+              left: 0;
+              right: 0;
+              background: rgba(25, 8, 0, 0.2);
+            `}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
