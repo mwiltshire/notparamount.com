751147752fdc7ad0cabde308ac1dff1545585a6d
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-06 23:54:45 +0200

    wip: container constraint

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,22 +1,32 @@
 import React, { FC } from 'react';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import { BP_MIN_LG } from '../breakpoints';
 
-const Container: FC = ({ children }) => (
-  <div
-    css={css`
-      margin-left: auto;
-      margin-right: auto;
-      padding: 0;
-      width: 100%;
+type ContainerProps = {
+  constrain?: boolean;
+};
+
+const ContainerDiv = styled.div<ContainerProps>`
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0;
+  width: 100%;
+  ${BP_MIN_LG} {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+  ${({ constrain }) =>
+    constrain
+      ? `
       ${BP_MIN_LG} {
-        padding-left: 30px;
-        padding-right: 30px;
-      }
-    `}
-  >
-    {children}
-  </div>
+        max-width: 720px;
+      }`
+      : ''}
+`;
+
+const Container: FC<ContainerProps> = ({ constrain = false, children }) => (
+  <ContainerDiv constrain={constrain}>{children}</ContainerDiv>
 );
 
 export default Container;
