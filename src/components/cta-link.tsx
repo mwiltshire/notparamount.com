75580df1cb01936e259812c4e5a587d421c0c3ef
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-13 17:22:29 +0200

    styles / smooth scroll

--- src/components/cta-link.tsx
+++ src/components/cta-link.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { css } from '@emotion/core';
 import { motion } from 'framer-motion';
+import { useScroll } from '../hooks';
 
 type CTALinkProps = {
   to: string;
@@ -8,49 +9,61 @@ type CTALinkProps = {
 
 const variants = {
   default: { x: -3, opacity: 0, scale: 0 },
-  hover: { x: 10, opacity: 1, scale: 1 }
+  hover: { x: 7, opacity: 1, scale: 1 }
 };
 
-const CTALink: FC<CTALinkProps> = ({ to, children }) => (
-  <motion.a
-    initial="default"
-    whileHover="hover"
-    css={css`
-      position: relative;
-      display: inline-flex;
-      align-items: center;
-      color: inherit;
-      text-decoration: none;
-    `}
-    href={to}
-  >
-    <span
+const CTALink: FC<CTALinkProps> = ({ to, children }) => {
+  const scrollTo = useScroll({ offset: -48 });
+  return (
+    <motion.a
+      initial="default"
+      whileHover="hover"
       css={css`
         position: relative;
-        display: flex;
+        display: inline-flex;
         align-items: center;
-        &:after {
-          content: '';
-          position: absolute;
-          bottom: -5px;
-          width: 100%;
-          height: 3px;
-          background: currentColor;
-        }
+        color: inherit;
+        text-decoration: none;
       `}
+      href={to}
+      onClick={e => {
+        e.preventDefault();
+        scrollTo(to);
+      }}
     >
-      {children}
-    </span>
-    <motion.svg variants={variants} viewBox="0 0 24 24" height="15">
-      <motion.path
+      <span
         css={css`
-          stroke: currentColor;
+          position: relative;
+          &:after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: -3px;
+            width: 100%;
+            height: 3px;
+            background: currentColor;
+          }
         `}
-        strokeWidth="2"
-        d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
-      />
-    </motion.svg>
-  </motion.a>
-);
+      >
+        {children}
+      </span>
+      <motion.svg
+        variants={variants}
+        viewBox="0 0 24 24"
+        height="15"
+        role="img"
+        aria-hidden
+      >
+        <motion.path
+          css={css`
+            stroke: currentColor;
+          `}
+          strokeWidth="2"
+          d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+        />
+      </motion.svg>
+    </motion.a>
+  );
+};
 
 export default CTALink;
