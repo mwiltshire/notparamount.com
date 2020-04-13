99707fccf07371681522be0c3cf228c617404692
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-13 15:21:17 +0200

    wip: toast component

---
+++ src/components/toast.tsx
@@ -0,0 +1,137 @@
+import React, { FC, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { css } from '@emotion/core';
+import { motion, AnimatePresence } from 'framer-motion';
+
+type ToastStatus = 'success' | 'error';
+
+type ToastProps = {
+  show: boolean;
+  message: string;
+  handleClose: () => void;
+  autoClose?: boolean;
+  status?: ToastStatus | null;
+};
+
+type ToastContainerProps = Omit<ToastProps, 'show'>;
+
+const ToastContainer: FC<ToastContainerProps> = ({
+  status,
+  message,
+  handleClose,
+  autoClose = true
+}) => {
+  useEffect(() => {
+    if (autoClose) {
+      const id = setTimeout(() => handleClose(), 5000);
+      return () => clearTimeout(id);
+    }
+  }, [autoClose, handleClose]);
+
+  const toastStatus: ToastStatus = status || 'success';
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: '-50%', y: 50, scale: 0.5 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      exit={{
+        opacity: [1, 1, 0],
+        y: [10, -20, 50],
+        transition: { duration: 0.3 }
+      }}
+      css={css`
+        position: fixed;
+        bottom: 1rem;
+        width: 500px;
+        background: ${toastStatus === 'success' ? '#effbf0' : '#ffdcdc'};
+        transform: translateX(-50%);
+        left: 50%;
+        border-radius: 0.3rem;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-basis: 3rem;
+            background: ${toastStatus === 'success' ? '#46b946' : '#ae4335'};
+            border-top-left-radius: 0.3rem;
+            border-bottom-left-radius: 0.3rem;
+            & svg {
+              height: 1.5rem;
+            }
+            & path {
+              fill: #fff;
+            }
+          `}
+        >
+          <svg role="img" viewBox="0 0 24 24">
+            {toastStatus === 'success' ? (
+              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.25 8.891l-1.421-1.409-6.105 6.218-3.078-2.937-1.396 1.436 4.5 4.319 7.5-7.627z" />
+            ) : (
+              <path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+            )}
+          </svg>
+        </div>
+        <p
+          css={css`
+            flex: 1;
+            margin: 0;
+            padding: 1rem;
+            color: ${toastStatus === 'success' ? '#46b946' : '#ae4335'};
+          `}
+        >
+          {message}
+        </p>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <button
+            aria-label="Close toast"
+            onClick={handleClose}
+            css={css`
+              background: none;
+              border: none;
+              cursor: pointer;
+              &:hover path {
+                fill: currentColor;
+              }
+              &:focus {
+                outline: none;
+              }
+              & svg {
+                height: 0.75rem;
+              }
+              & path {
+                transition: fill 300ms ease;
+                fill: #afafaf;
+              }
+            `}
+          >
+            <svg role="img" viewBox="0 0 24 24">
+              <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+const Toast: FC<ToastProps> = ({ show, ...rest }) => {
+  return createPortal(
+    <AnimatePresence>{show && <ToastContainer {...rest} />}</AnimatePresence>,
+    document.body
+  );
+};
+
+export default Toast;
