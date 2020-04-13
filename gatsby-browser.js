54ba3cdc6b120984a154fa898a241cc29b77e411
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-13 17:24:09 +0200

    smooth scroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import smoothscroll from 'smoothscroll-polyfill';
 
-// You can delete this file if you're not using it
+export const onClientEntry = () => {
+  smoothscroll.polyfill();
+};
