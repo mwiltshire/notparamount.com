90473af89e22f4afa597dea9401787379459b535
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-13 17:22:43 +0200

    toast

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
 import Section from './section';
 import Container from './container';
 import ContactForm from './contact-form';
+import Toast from './toast';
 import { Theme } from './layout';
 
 const Contact = () => {
   const theme = useTheme<Theme>();
+  const [status, setSubmissionStatus] = useState<'success' | 'error' | null>(
+    null
+  );
+
   return (
     <Section id="contact" heading="Contact" background="backgroundGray">
       <Container constrain>
@@ -18,8 +23,21 @@ const Contact = () => {
         >
           Let’s work together
         </h3>
-        <ContactForm />
+        <ContactForm
+          onSuccess={() => setSubmissionStatus('success')}
+          onError={() => setSubmissionStatus('error')}
+        />
       </Container>
+      <Toast
+        show={!!status}
+        status={status}
+        handleClose={() => setSubmissionStatus(null)}
+        message={
+          status === 'success'
+            ? '🙌 Got it! I’ll get back to you soon!'
+            : '😱 Gasp! Something went wrong. Please try again later!'
+        }
+      />
     </Section>
   );
 };
