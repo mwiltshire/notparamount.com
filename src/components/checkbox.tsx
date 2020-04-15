1189296b373476a4acbbee6f306056fee0677d12
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-15 21:06:52 +0200

    checkbox focus / theme

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -27,18 +27,23 @@ const Checkbox: FC<CheckboxProps> = ({ label, ...props }) => {
           margin-bottom: 1rem;
           & label {
             margin-left: 0.5rem;
-            font-size: 14px;
+            font-size: ${theme.fontSizes.sm};
           }
           & svg {
             flex: 0 0 1.3rem;
             height: 1.3rem;
-            border: 2px solid
-              ${meta.error && meta.touched
-                ? theme.colors.error
-                : theme.colors.backgroundDark};
+            ${!field.checked &&
+              `border: 2px solid
+              ${
+                meta.error && meta.touched
+                  ? theme.colors.red300
+                  : theme.colors.gray100
+              };`}
             border-radius: 3px;
-            transition: background 300ms ease;
-            background: ${field.checked ? '#3dc1a1' : 'transparent'};
+            transition: all 300ms ease;
+            background: ${field.checked
+              ? theme.colors.green200
+              : theme.colors.white};
           }
           & path {
             transition: opacity 300ms ease;
@@ -50,34 +55,37 @@ const Checkbox: FC<CheckboxProps> = ({ label, ...props }) => {
           }
         `}
       >
+        <input
+          id={props.name}
+          value={props.name}
+          type="checkbox"
+          {...field}
+          {...props}
+          css={css`
+            position: absolute;
+            left: 0;
+            top: 0;
+            height: 100%;
+            height: 1.3rem;
+            width: 1.3rem;
+            opacity: 0;
+            margin: 0;
+            padding: 0;
+            cursor: pointer;
+            &:focus + svg {
+              border: 2px solid ${theme.colors.black};
+            }
+          `}
+        />
         <svg viewBox="0 0 20 20">
           <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" />
         </svg>
         <label htmlFor={props.name}>{label}</label>
       </div>
-      <input
-        id={props.name}
-        value={props.name}
-        type="checkbox"
-        {...field}
-        {...props}
-        css={css`
-          position: absolute;
-          left: 0;
-          top: 0;
-          height: 100%;
-          height: 1.3rem;
-          width: 1.3rem;
-          opacity: 0;
-          margin: 0;
-          padding: 0;
-          cursor: pointer;
-        `}
-      />
       {meta.error && meta.touched && (
         <p
           css={css`
-            color: ${theme.colors.error};
+            color: ${theme.colors.red300};
             margin-bottom: 1rem;
           `}
         >
@@ -86,7 +94,7 @@ const Checkbox: FC<CheckboxProps> = ({ label, ...props }) => {
               font-size: ${theme.fontSizes.sm};
               border-radius: 0.3rem;
               padding: 0.3rem;
-              background: #ffdcdc;
+              background: ${theme.colors.red100};
             `}
           >
             {meta.error}
